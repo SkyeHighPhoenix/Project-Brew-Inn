3d878ce5e4f5c3a3d576145adab246a8196ec822
Surfdog2005 <williampthurston@gmail.com>
2025-02-04 20:51:23 +0000

    made the time speed adjustable

--- Source Code/brew_inn/dayNightCycle.gd
+++ Source Code/brew_inn/dayNightCycle.gd
@@ -1,12 +1,18 @@
 extends CanvasModulate
-var time = 0.00
+
+const minsPerDay = 1440
+const minsPerHour = 60
+const minuteDuration = (2 * PI) / minsPerDay
+
+var time = 0.0	
 
 @export var gradient:GradientTexture1D
-
+@export var timeSpeed = 10.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	time += delta
+	time += delta * minuteDuration * timeSpeed
 	var value = (sin(time - PI/2) + 1.0) / 2.0
+	
 	self.color = gradient.gradient.sample(value)
 	
