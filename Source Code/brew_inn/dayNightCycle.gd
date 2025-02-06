7334511efe700593c0b151d73f62abe55f4136f3
Surfdog2005 <williampthurston@gmail.com>
2025-02-06 21:48:56 +0000

    updated day night cycle

--- Source Code/brew_inn/dayNightCycle.gd
+++ Source Code/brew_inn/dayNightCycle.gd
@@ -7,12 +7,13 @@ const minuteDuration = (2 * PI) / minsPerDay
 var time = 0.0	
 
 @export var gradient:GradientTexture1D
-@export var timeSpeed = 10.0
+@export var timeSpeed = 1.0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	time += delta * minuteDuration * timeSpeed
-	var value = (sin(time - PI/2) + 1.0) / 2.0
-	
+	time += delta * minuteDuration * timeSpeed;
+	var value = (sin(time-0.5*PI) + 1.0) / 2.0
+	print(time)
+	print(value)
 	self.color = gradient.gradient.sample(value)
 	
