b7f4163212f94c789f480961028a68526fe9433f
Surfdog2005 <williampthurston@gmail.com>
2025-02-26 13:27:18 +0000

    global tick system and base farm first version

---
+++ Source Code/brew_inn/Scripts/base_farm.gd
@@ -0,0 +1,28 @@
+extends Node2D
+var tick = 0
+var ticksToGrow = 100
+var resourcesOnHarvest = 25
+var localStorage = true
+var storageCap = 500
+var resourceType = "wheat"
+var storedResources = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	GlobalTick.tickIncrease.connect(tickIncrease)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if storedResources > storageCap:
+		storedResources = storageCap
+
+func increaseResources():
+	if tick % ticksToGrow == 0:
+		if localStorage == true:
+			storedResources += resourcesOnHarvest
+			print(storedResources)
+
+func tickIncrease():
+	tick += 1
+	increaseResources()
