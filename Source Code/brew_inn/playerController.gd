ed673fb9a08bef86a1212dd71e89dfe18271d2da
Surfdog2005 <williampthurston@gmail.com>
2025-04-08 14:42:37 +0100

    player controller test scene done

--- Source Code/brew_inn/playerController.gd
+++ Source Code/brew_inn/playerController.gd
@@ -1,13 +1,33 @@
-extends StaticBody3D
+extends CharacterBody3D
+@export var sensitivity =1.0
+@export var speed = 3.0
 
-
+@onready var camera = $Camera3D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * sensitivity)
+		camera.rotate_x(-event.relative.y * sensitivity)
+		camera.transform = camera.transform.orthonormalized()
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-90), deg_to_rad(65))
+		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-##NOTE TO SELF: https://www.youtube.com/watch?v=AW3rT-7J8ag
+func _physics_process(delta: float) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	var input_dir = Input.get_vector("left", "right", "forward", "backward")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	
+	if direction:
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		print (velocity.x)
+	else:
+		velocity.x = 0.0
+		velocity.z = 0.0
+		print (velocity.x)
+		
+	move_and_slide()
