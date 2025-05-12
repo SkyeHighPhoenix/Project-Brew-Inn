cafc8183e0e5e63c95c6f4e0f073eb16a6c6631a
Deanosaur <67189400+Deanosauree@users.noreply.github.com>
2025-05-12 12:59:51 +0100

    quick changes

--- Source Code/brew_inn/Scripts/camera_2d.gd
+++ Source Code/brew_inn/Scripts/camera_2d.gd
@@ -2,7 +2,7 @@ extends Camera2D
 const zoomSpeed = 0.1
 const borders = 500
 var tileSize = 128
-
+var outside = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -14,16 +14,18 @@ func _process(delta: float) -> void:
 	pass
 
 func _input(event: InputEvent):
-	if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
-		position -= event.relative/zoom
-		restrictBorders()
-		print(position)
-	if event is InputEventMouseButton:
-		if Input.is_action_just_pressed('ScrollUp'):
-			zoom *= 1+zoomSpeed
-		elif Input.is_action_just_pressed("ScrollDown"):
-			if zoom.x >= 0.1:
-				zoom *= 1-zoomSpeed
+	if outside:
+		if event is InputEventMouseMotion and Input.is_mouse_button_pressed(MOUSE_BUTTON_MIDDLE):
+			position -= event.relative/zoom
+			restrictBorders()
+			print(position)
+		if event is InputEventMouseButton:
+			if Input.is_action_just_pressed('ScrollUp'):
+				if zoom.x <= 10:
+					zoom *= 1+zoomSpeed
+			elif Input.is_action_just_pressed("ScrollDown"):
+				if zoom.x >= 0.1:
+					zoom *= 1-zoomSpeed
 
 func restrictBorders():
 	var actualBorderX = (borders*tileSize)-(get_viewport_rect().size.x/2)/zoom.x
